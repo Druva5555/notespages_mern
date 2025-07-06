fb70e96ad195f1e09cac46ecac56f10992a57843
Druva5555 <dhruvasaithanuku@gmail.com>
2025-07-06 23:29:25 +0530

    prepared deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dontenv from "dotenv";
 import cors from "cors";
+import path from "path";
 
 import noteroutes from "./routes/notesroutes.js";
 import { connectDB } from "./config/db.js";
@@ -11,20 +12,33 @@ dontenv.config();
 
 const app = express();
 const port = process.env.port || 3000;
+const __dirname = path.resolve();
 
 connectDB();
 
 
-app.use(cors({
-  origin: "http://localhost:5173",
-  credentials: true,
-}));
+if(process.env.NODE_ENV !== "production"){
+  app.use(cors({
+    origin: "http://localhost:5173",
+  }));
+}
+
 
 app.use(express.json());
 app.use(rateLimiter);
 
+
 app.use("/api/notes", noteroutes);
 
+if(process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "../frontend/dist/index.html")); 
+
+})
+
+}
 app.listen(port, () => {
   console.log("Server is running on port",port);
 });
